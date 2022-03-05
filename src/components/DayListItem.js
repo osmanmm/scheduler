b3d4d90cf25b0e7a6e5a381b9986eb9b5411ba6a
Mohamed Osman <84829054+osmanmm@users.noreply.github.com>
2022-03-05 11:30:22 -0500

    add data-testid to DayListItem componant

---
+++ src/components/DayListItem.js
@@ -0,0 +1,34 @@
+import React from "react"
+import classNames from "classnames"
+import "components/DayListItem.scss"
+
+export default function DayListItem(props) {
+  const dayClass = classNames({
+    "day-list__item": true,
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0,
+  })
+
+  const formatSpots = props => {
+    return (
+      <div>
+        {props.spots === 0 && (
+          <h3 className="text--light">no spots remaining</h3>
+        )}
+        {props.spots === 1 && (
+          <h3 className="text--light">{props.spots} spot remaining</h3>
+        )}
+        {props.spots > 1 && (
+          <h3 className="text--light">{props.spots} spots remaining</h3>
+        )}
+      </div>
+    )
+  }
+
+  return (
+    <li className={dayClass} onClick={() => props.setDay()} data-testid="day">
+      <h2 className="text--regular">{props.name}</h2>
+      {formatSpots(props)}
+    </li>
+  )
+}
